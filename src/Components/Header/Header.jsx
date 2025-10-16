b3fdf1188f04fd6b15e82c4260da14b164a173e7
Ashan Kavinda <bandaralamak.23@uom.lk>
2025-10-16 11:39:22 +0530

    v

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -7,10 +7,6 @@ import '../../SCSS/Header.scss'
 function Header() {
   return (
     <header className="app-header">
-      <div className="brand">
-        <img src="https://rcunialumni.home.blog/wp-content/uploads/2023/05/racualu-full-black-logo.png?w=1080" alt="Rotaract logo" className="logo" />
-      </div>
-
       <div className="header-actions">
         <Link to="/calendar" className="icon-btn" aria-label="Calendar"><FaCalendar/></Link>
         <Link to="/signout" className="icon-btn" aria-label="Sign out"><FaSignOutAlt/></Link>
--- src/Pages/Chair/ManageCommitees.jsx
+++ src/Pages/Chair/ManageCommitees.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
 import Header from '../../Components/Header/Header'
+import LeftNavChair from '../../Components/chair Component/LeftNavChair'
+import SearchAndButton from '../../Components/SerachAnd/SearchAndButton'
+//
 
 const ManageCommitees = () => {
   return (
+    <>
 <Header />
+<SearchAndButton title1="Add Committees" title2="View Committees"/>
+<LeftNavChair />
+
+{/* <ProjectsTable /> */}
+
+</>
   )
 }
 
