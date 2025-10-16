024e36b4005135c7d44f872a5dff2eb2a5c38c16
Ashan Kavinda <bandaralamak.23@uom.lk>
2025-10-16 17:47:52 +0530

    a

--- src/main.jsx
+++ src/main.jsx
@@ -20,41 +20,7 @@ const router= createBrowserRouter([
     element: <ManageCommitees/>,
     errorElement:<div>Page Not Found</div>
   },
-  // {
-  //   path:"/Profile",
-  //   element:<Profile/>,
-  //   errorElement:<div>Page Not Found</div>
-  // },
-  // {
-  //   path:"/My Projects",
-  //   element:<MyProjects/>,
-  //   errorElement:<div>Page Not Found</div>
-  // },
-  // {
-  //   path:"/Rotaract",
-  //   element:<Rotaract/>,
-  //   errorElement:<div>Page Not Found</div>
-  // },
-  // {
-  //   path:"/Manage",
-  //   element:<Manage/>,
-  //   errorElement:<div>Page Not Found</div>
-  // },
-  // {
-  //   path:"/Committees",
-  //   element:<Committees/>,
-  //   errorElement:<div>Page Not Found</div>
-  // },
-  // {
-  //   path:"/Reports",
-  //   element:<Reports/>,
-  //   errorElement:<div>Page Not Found</div>
-  // },
-  // {
-  //   path:"/Settings",
-  //   element:<Settings/>,
-  //   errorElement:<div>Page Not Found</div>
-  // }
+ 
   {
     path:"/ManageTasks",
     element:<ManageTask/>,
@@ -62,16 +28,9 @@ const router= createBrowserRouter([
   },
 ])
 
-import AdminProjects from './Pages/Admin/AdminProjects.jsx'
-import AdminCommittees from './Pages/Admin/AdminCommitees.jsx'
-import AdminDashboard from './Pages/Admin/AdminDashboard.jsx'
-
-
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    {/* <RouterProvider router={router}/> */}
-    <AdminDashboard/>
-
+     <RouterProvider router={router}/> 
   </StrictMode>,
 )
