e54182298382ac55429b817fb6ab472354210186
Banula Peiris <banulapeiris1uni@gmail.com>
2025-10-17 15:34:34 +0530

    modify main

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import Test from './Pages/Test.jsx'
 import ManageCommitees from './Pages/Chair/ManageCommitees.jsx'
 import ManageTask from './Pages/Chair/ManageTask.jsx'
+import AdminProjects from './Pages/Admin/AdminProjects.jsx'
 
 
 
@@ -20,58 +21,22 @@ const router= createBrowserRouter([
     element: <ManageCommitees/>,
     errorElement:<div>Page Not Found</div>
   },
-  // {
-  //   path:"/Profile",
-  //   element:<Profile/>,
-  //   errorElement:<div>Page Not Found</div>
-  // },
-  // {
-  //   path:"/My Projects",
-  //   element:<MyProjects/>,
-  //   errorElement:<div>Page Not Found</div>
-  // },
-  // {
-  //   path:"/Rotaract",
-  //   element:<Rotaract/>,
-  //   errorElement:<div>Page Not Found</div>
-  // },
-  // {
-  //   path:"/Manage",
-  //   element:<Manage/>,
-  //   errorElement:<div>Page Not Found</div>
-  // },
-  // {
-  //   path:"/Committees",
-  //   element:<Committees/>,
-  //   errorElement:<div>Page Not Found</div>
-  // },
-  // {
-  //   path:"/Reports",
-  //   element:<Reports/>,
-  //   errorElement:<div>Page Not Found</div>
-  // },
-  // {
-  //   path:"/Settings",
-  //   element:<Settings/>,
-  //   errorElement:<div>Page Not Found</div>
-  // }
+ 
   {
     path:"/ManageTasks",
     element:<ManageTask/>,
     errorElement:<div>Page Not Found</div>
   },
+  {
+    path:"/AdminProjects",
+    element:<AdminProjects/>,
+    errorElement:<div>Page Not Found</div>
+  }
 ])
-
-import AdminProjects from './Pages/Admin/AdminProjects.jsx'
-import AdminCommittees from './Pages/Admin/AdminCommitees.jsx'
-import AdminDashboard from './Pages/Admin/AdminDashboard.jsx'
-
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    {/* <RouterProvider router={router}/> */}
-    <AdminDashboard/>
-
+    <RouterProvider router={router}/>
   </StrictMode>,
 )
