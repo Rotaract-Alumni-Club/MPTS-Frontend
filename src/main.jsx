ed78090c383a98e34fa26b05ac215f28111c71f4
Banula Peiris <banulapeiris1uni@gmail.com>
2025-10-18 18:57:16 +0530

    modify main

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
+import { BrowserRouter } from "react-router-dom";
 import Test from './Pages/Test.jsx'
 import ManageCommitees from './Pages/Chair/ManageCommitees.jsx'
 import ManageTask from './Pages/Chair/ManageTask.jsx'
